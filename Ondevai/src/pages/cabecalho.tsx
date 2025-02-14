4226745c00d8bcd967598d91e9c69772a7916a13
LUCAS BORNATHO PIRES <lucas.bpires4@senacsp.edu.br>
2025-02-13 22:04:15 -0300

    alteracao no cabecalho -lucasbornatho

--- Ondevai/src/pages/cabecalho.tsx
+++ Ondevai/src/pages/cabecalho.tsx
@@ -1,12 +1,14 @@
 import '../estilo.css'
+import Menu from './menu'
 
 function Cabecalho () {
     return (
-    <div className='cabecalho'>
-        <div className='logo-home'>
-            <img src='./Logo.png' alt='Logo' />
+        <div className='cabecalho'>
+            <div className='logo-home'>
+                <img src='./Logo.png' alt='Logo' />
+            </div>
+                <Menu />
         </div>
-    </div>
     )
 }
 
--- Ondevai/src/pages/home.tsx
+++ Ondevai/src/pages/home.tsx
@@ -5,7 +5,6 @@ function Home () {
     return (
         <div className='home'>
           <Cabecalho />
-          <Menu />
         </div>
     )
 }
