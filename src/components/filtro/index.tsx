c1f77b71815572e24840f0cdc28b1c31852f55eb
ribrafa <rafaelribeiro.code@gmail.com>
2025-02-24 11:15:06 -0300

    Criei um componente filtro e fixei ele na pagina home para acompanhar a tela dos cards

---
+++ src/components/filtro/index.tsx
@@ -0,0 +1,18 @@
+function Filtro() {
+    return (
+      <div className="container-filtro">
+        <h2>Filtros</h2>
+        <ul>
+          <li>ROCK</li>
+          <li>RAP</li>
+          <li>SAMBA</li>
+          <li>FUNK</li>
+          <li>PAGODE</li>
+          <li>SERTANEJO</li>
+          <li>MPB</li>
+        </ul>
+      </div>
+    );
+}
+
+export default Filtro;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,17 @@
 import Rodape from "../../components/rodape"
 import Cabecalho from "../../components/cabecalho"
 import CardEvento from "../../components/cardevento"
+import Filtro from "../../components/filtro"
 
 function Home () {
     return (
         <div className='home'>
             <Cabecalho />
-            <br />
+            <Filtro />
             <CardEvento />
-            <br />
             <CardEvento />
-            <br />
             <CardEvento />
-            <br />
             <CardEvento />
-            <br />
             <Rodape />
         </div>
     )
