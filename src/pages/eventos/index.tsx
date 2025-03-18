c7f1fc854363bebcf2b643bf1b68f66dcaac3dcb
Lucas Bornatho Pires <lucasbornatho@outlook.com>
2025-03-18 20:18:02 -0300

    Renderizacao por lista no Eventos

--- src/pages/eventos/index.tsx
+++ src/pages/eventos/index.tsx
@@ -4,6 +4,12 @@ import CardEvento from "../../components/cardevento"
 import Filtro from "../../components/filtro"
 
 function Eventos () {
+
+    const eventos = [{ id: 1, titulo: "Os caras malvados", imgUrl: "/capa-evento/ImagemCapaEvento.png" },
+    { id: 2, titulo: "Os caras muito malvados", imgUrl: "/capa-evento/ImagemCapaEvento.png" },
+    { id: 3, titulo: "Os caras muito muito malvados", imgUrl: "/capa-evento/ImagemCapaEvento.png" },
+    { id: 4, titulo: "Os caras malvadosssssssssssss", imgUrl: "/capa-evento/ImagemCapaEvento.png" }];
+
     return (
         <div className='home'>
             <Cabecalho />
@@ -11,10 +17,11 @@ function Eventos () {
             <div className='nome-pagina-eventos'>
                 <h1>Eventos</h1>
             </div>
-            <CardEvento imgUrl='/capa-evento/ImagemCapaEvento.png' titulo="Os caras malvados"/>
-            <CardEvento imgUrl='/capa-evento/ImagemCapaEvento.png' titulo="Os caras malvados"/>
-            <CardEvento imgUrl='/capa-evento/ImagemCapaEvento.png' titulo="Os caras malvados"/>
-            <CardEvento imgUrl='/capa-evento/ImagemCapaEvento.png' titulo="Os caras malvados"/>
+            <ul>
+                {eventos.map(evento => 
+                <li key={evento.id}><CardEvento titulo={evento.titulo} imgUrl={evento.imgUrl} /></li>
+                )}
+            </ul>
             <Rodape />
         </div>
     )
