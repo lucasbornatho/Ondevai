b660c125409e17197296183ae66ba01f3ad25a3f
Marcio Bincoleto <marciobincoletojunior@gmail.com>
2025-03-23 23:20:42 -0300

    Feito correções no banner do cardhome, add selector no filtro da pagina eventos(genero e cidade) e também add input para selecionar uma data, alterado o botão de A-Z para um botão de cclique

--- src/components/cardhome/index.tsx
+++ src/components/cardhome/index.tsx
@@ -1,90 +1,55 @@
+import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
-type CardHomeProps = {
-    imgUrl: string;
-    evento: string;
+interface Evento {
+  nome: string;
+  imgUrl: string;
+  link: string;
 }
+
+const eventos: Evento[] = [
+  { nome: "Eletrônica", imgUrl: "./banner/Eletronica.png", link: "/eventos" },
+  { nome: "Hip Hop", imgUrl: "./banner/hip hop.png", link: "/eventos" },
+  { nome: "Rock", imgUrl: "./banner/Rock.png", link: "/eventos" },
+  { nome: "Samba", imgUrl: "./banner/Samba.png", link: "/eventos" },
+  { nome: "Teatro", imgUrl: "./banner/Teatro.png", link: "/eventos" },
+  { nome: "Bares", imgUrl: "./banner/Bares.png", link: "/eventos" },
+];
 
 const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-  
-function CardHome() {
-    return (
-        <div>
-            <div className="menu-netflix">
-                <Carousel  swipeable={false}
-                            draggable={false}
-                            showDots={true}
-                            responsive={responsive}
-                            ssr={true} // means to render carousel on server-side.
-                            infinite={true}
-                            autoPlaySpeed={1000}
-                            keyBoardControl={true}
-                            customTransition="all .5"
-                            transitionDuration={500}
-                            containerClass="carousel-container"
-                            removeArrowOnDeviceType={["tablet", "mobile"]}
-                            dotListClass="custom-dot-list-style"
-                            itemClass="carousel-item-padding-40-px">
-                    
-                      <a href="./"><img src="./banner/Eletronica.png" alt="Eletronica" /></a>
-                      <a href="./"><img src="./banner/hip hop.png" alt="Hip Hop" /></a>
-                      <a href="./"><img src="./banner/Rock.png" alt="Rock" /></a>
-                      <a href="./"><img src="./banner/Samba.png" alt="Samba" /></a>
-                      <a href="./"><img src="./banner/Teatro.png" alt="Teatro" /></a>
-                      <a href="./"><img src="./banner/Bares.png" alt="Bares" /></a>
-                    
-                    
-                </Carousel>
-            </div>
-        </div>
-    );
-}
+  superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
+  desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+  tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+  mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
+};
 
-export default CardHome;
+const CardHome: React.FC = () => {
+  return (
+    <div className="menu-netflix">
+      <Carousel
+        swipeable
+        draggable
+        showDots
+        responsive={responsive}
+        ssr
+        infinite
+        autoPlaySpeed={1000}
+        keyBoardControl
+        customTransition="all .5s"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        {eventos.map((evento) => (
+          <a key={evento.nome} href={evento.link} target="_blank" rel="noopener noreferrer">
+            <img src={evento.imgUrl} alt={evento.nome} />
+          </a>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
-// function CardHome () {
-//     return(
-//         <div>
-//             <div className="menu-netflix">
-
-//                 <div className="netflix-menu-card">
-//                     <a href="./"><img src="./banner/Eletronica.png" alt="" /></a>
-//                     <a href="./"><img src="./banner/hip hop.png" alt="" /></a>
-//                     <a href="./"><img src="./banner/Rock.png" alt="" /></a>
-//                     <a href="./"><img src="./banner/Samba.png" alt="" /></a>
-//                     <a href="./"><img src="./banner/Teatro.png" alt="" /></a>
-//                     <a href="./"><img src="./banner/Bares.png" alt="" /></a>
-//                 </div>
-
-//                 {/* <div className="menu-netflix-cards">
-//                     <img src={imgUrl} alt={evento}></img>
-//                     <div>
-//                         <p>{evento}</p>
-//                     </div>
-//                 </div> */}
-//             </div>
-        
-//         </div>
-//     )
-// }
-
-// export default CardHome
+export default CardHome
--- src/components/filtro/index.tsx
+++ src/components/filtro/index.tsx
@@ -1,21 +1,34 @@
 function Filtro() {
     return (
       <div className="container-filtro">
-        <div className="filtro-botao">
-          <p>A - Z</p>
+        <div>
+          <input className="filtro-botao" type="date"/>
+        </div>
+        <div>
+          <select className="filtro-botao">
+              <option value="0">Genero</option>
+              <option value="1">Rock</option>
+              <option value="2">Teatro</option>
+              <option value="3">Bares</option>
+              <option value="4">Resturantes</option>
+              <option value="5">Festas</option>
+              <option value="6">Shows</option>
+          </select>
+        </div>
+        
+        <div>
+          <select className="filtro-botao">
+            <option value="0">Cidade</option>
+            <option value="1">Bauru</option>
+            <option value="2">Piratininga</option>
+            <option value="3">Pederneiras</option>
+            <option value="4">Agudos</option>
+          </select>
+        </div>
+        
+        <div>
+          <button className="filtro-botao css-botao">Filtrar</button>
                 {/* <Link to='/' className='link-botao-menu'> Home </Link> */}
-        </div>
-        <div className="filtro-botao">
-          <p>Data</p>
-                {/* <Link to='/eventos' className='link-botao-menu'> Eventos </Link> */}
-        </div>
-        <div className="filtro-botao">
-          <p>Genero</p>
-                {/* <Link to='/sobre' className='link-botao-menu'> Sobre </Link> */}
-        </div>
-        <div className="filtro-botao">
-          <p>Cidade</p>
-                {/* <Link to='/sobre' className='link-botao-menu'> Sobre </Link> */}
         </div>
       </div>
     );
