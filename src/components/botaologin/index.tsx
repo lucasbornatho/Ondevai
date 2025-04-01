c91df86c7d4c741e1068b600735c2c457927e83b
DjalmaMansueto <djalma.mansueto@gmail.com>
2025-04-01 18:39:02 -0300

    alterando nome das divs, do CadastroUsuario

--- src/components/botaologin/index.tsx
+++ src/components/botaologin/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom"
-
 function BotaoLogin() {
   const [LoginVisivel, setLoginVisivel] = useState(false);
 
--- src/pages/cadastrousuario/index.tsx
+++ src/pages/cadastrousuario/index.tsx
@@ -2,7 +2,7 @@ import Cabecalho from "../../components/cabecalho"
 import Rodape from "../../components/rodape"
 import { useState } from "react";
 import "../../estilo/estilo.css"
-
+import "../../components/botaologin"
 
 function CadastroUsuario() {
     // const [LoginVisivel, setLoginVisivel] = useState(false);
@@ -11,47 +11,49 @@ function CadastroUsuario() {
     //   setLoginVisivel(!LoginVisivel);
     // }
   
-    return (<div>
+    return (
+    <div>
     <Cabecalho/>
         <div className="CadastroUsuario">
         
-          <div className="botaologin-container">
+          <div className="CadastroUsuario-Container">
           {/* <button onClick={abrirCadastroUsuario} className="Criar Cadastro" >
           <img className='imagembotaologin' src="Icone Login.png" alt="" />
           </button>
               
             {LoginVisivel && ( */}
-              <div className="botaologin-form">
+              <div className="botaocadastrologin-form">
                 <h1>Onde vai Cadastro</h1>
                 <img className='fotologin' src="Icone Login.png" alt="login" />
                 <form>
-                  <div className="botaologin-form-div1">
-                    <input className="botaologin-form-input" type="text" placeholder="Nome Completo"/>
+                  <div className="botaocadastrologin-form-div1">
+                    <input className="botaocadastrologin-form-input" type="text" placeholder="Nome Completo"/>
                   </div>
-                  <div className="botaologin-form-div1">
-                    <input className="botaologin-form-input" type="text" placeholder="Email"/>
+                  <div className="botaocadastrologin-form-div1">
+                    <input className="botaocadastrologin-form-input" type="text" placeholder="Email"/>
                   </div>
-                  <div className="botaologin-form-div1">
-                    <input className="botaologin-form-input" type="password" placeholder="Senha"/>
+                  <div className="botaocadastrologin-form-div1">
+                    <input className="botaocadastrologin-form-input" type="password" placeholder="Senha"/>
                   </div>
-                  <div className="botaologin-form-div1">
-                    <input className="botaologin-form-input" type="text" placeholder="Telefone"/>
+                  <div className="botaocadastrologin-form-div1">
+                    <input className="botaocadastrologin-form-input" type="text" placeholder="Telefone"/>
                   </div>
   
-                  <div className="botaologin-form-div1">
-                    <input className="botaologin-form-input" type="text" placeholder="Sexo"/>
+                  <div className="botaocadastrologin-form-div1">
+                    <input className="botaocadastrologin-form-input" type="text" placeholder="Sexo"/>
                   </div>
   
                   <button className="botaologin-submit" type="submit" >Cadastre-se</button>
                 </form>
   
                
-             <Rodape/>
+            
                 </div>
              
             {/* )} */}
-          </div>
-     </div></div>    )
+          </div>  
+        <Rodape/>   </div> 
+     </div>    )
      }
   
 
