4527b0d6d56244811eafffc7df8c1303403f83de
ribrafa <rafaelribeiro.code@gmail.com>
2025-04-02 19:51:22 -0300

    Alterando a page do cadastro usuario e chamado o componente(ainda vamos alterar as nomenclaturas, apenas teste)

--- src/pages/cadastrousuario/index.tsx
+++ src/pages/cadastrousuario/index.tsx
@@ -1,61 +1,22 @@
 import Cabecalho from "../../components/cabecalho"
 import Rodape from "../../components/rodape"
-import { useState } from "react";
+
 import "../../estilo/estilo.css"
 import "../../components/botaologin"
+import CadastroUsuario from "../../components/cadastrousuario";
 
-function CadastroUsuario() {
-    // const [LoginVisivel, setLoginVisivel] = useState(false);
-  
-    // const CadastrarUsuario = () => {
-    //   setLoginVisivel(!LoginVisivel);
-    // }
+function CadastroUsuariopage() {
   
     return (
-    <div>
-    
-        <div className="CadastroUsuario">
+      <div>
         <Cabecalho/>
-          <div className="CadastroUsuario-Container">
-          {/* <button onClick={abrirCadastroUsuario} className="Criar Cadastro" >
-          <img className='imagembotaologin' src="Icone Login.png" alt="" />
-          </button>
-              
-            {LoginVisivel && ( */}
-              <div className="botaocadastrologin-form">
-                <h1>Onde vai Cadastro</h1>
-                <img className='fotologin' src="Icone Login.png" alt="login" />
-                <form>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Nome Completo"/>
-                  </div>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Email"/>
-                  </div>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="password" placeholder="Senha"/>
-                  </div>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Telefone"/>
-                  </div>
-  
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Sexo"/>
-                  </div>
-  
-                  <button className="botaologin-submit" type="submit" >Cadastre-se</button>
-                </form>
-  
-                 <Rodape/>
-           
-                </div>
-            
-            {/* )} */}
-          </div>  
-        
-        </div></div>    )
+        <CadastroUsuario/>
+        <Rodape/>
+
+      </div>
+        )
      }
   
 
 
-export default CadastroUsuario
+export default CadastroUsuariopage
