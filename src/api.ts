28b3a3852ab3b565e1117430d420c100baae63d4
Lucas Bornatho Pires <lucasbornatho@yahoo.com.br>
2025-04-07 22:16:00 -0300

    Iniciando a parte das funçoes para utilizacao da API

---
+++ src/api.ts
@@ -0,0 +1,55 @@
+export const api = {
+
+    CarregarTodosEventos: async () => {
+        let response = await fetch('')
+        let json = await response.json();
+        return json;
+    },
+
+    AdicionarUsuarios: async (title: string, body: string, userID: number) => {
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                body,
+                userID
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        let json = await response.json();
+        console.log(json);
+        return json;
+    },
+
+    AdicionarEventos: async (id: string, nome: string, genero: string, descricao: string, localizacao: string, image: string) => {
+        let response = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify({
+                id,
+                nome,
+                genero,
+                descricao,
+                localizacao,
+                image,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        let json = await response.json();
+        console.log(json);
+        return json;
+    },
+
+    //func vai filtrar conforme o parametro informado no filto/eventos
+    CarregarEventoUnico: async (param:string) => {
+        let response = await fetch('');
+        let json = await response.json();
+        return json
+    }
+    
+}
