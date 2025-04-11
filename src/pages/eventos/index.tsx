2dd4cad389ccf79e6f7d07505616906bfe75cc43
Lucas Bornatho Pires <lucasbornatho@yahoo.com.br>
2025-04-10 22:10:09 -0300

    Fazendo validações para captação de erro no carregarEventos na pagina eventos

--- src/pages/eventos/index.tsx
+++ src/pages/eventos/index.tsx
@@ -9,20 +9,26 @@ import { api } from "../../api"
 function Eventos () {
 
 
-    useEffect(() => { carregarTodosEventos() }, []);
+    useEffect(() => { carregarEventos() }, []);
 
 
     const [eventos, setEventos] = useState<CardEventoType[]>([]);
     const[loading, setLoading] = useState(false);
  
-    const carregarTodosEventos = async () => {
-        setLoading(true)
-
-        let json = await api.CarregarTodosEventos();
-
-        const dataArray = Array.isArray(json) ? json : [json];
+    const carregarEventos = async () => {
+        setLoading(true);
+    
+        try {
+            let json = await api.CarregarTodosEventos();
+            console.log(json);  // Apenas para verificar se está carregando os eventos
+            
+            const dataArray = Array.isArray(json) ? json : [json];
+            setEventos(dataArray);
+        } catch (error) {
+            console.error("Erro ao carregar os eventos:", error);
+        }
+    
         setLoading(false);
-        setEventos(dataArray);
     }
 
     return (
@@ -32,11 +38,15 @@ function Eventos () {
             <div className='nome-pagina-eventos'>
                 <h1>Eventos</h1>
             </div>
+            {loading ? <p>Carregando...</p> : (
             <ul>
-                {eventos.map(evento => 
-                    <li key={evento.id}><CardEvento nome={evento.nome} image={evento.image} localizacao={evento.localizacao} /></li>
-                )}
+                {eventos.map(evento => (
+                <li key={evento.id}>
+                <CardEvento nome={evento.nome} image={evento.image} localizacao={evento.localizacao} />
+                </li>
+                ))}
             </ul>
+)}
             <Rodape />
         </div>
     )
