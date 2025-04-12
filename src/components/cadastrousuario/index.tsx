507bf2ebbebe834884b973b82cce840dd2b1ade2
Marcio Bincoleto <marciobincoletojunior@gmail.com>
2025-04-11 23:05:04 -0300

    Removido pacote React-Elastic-Carousel do projeto

--- src/components/cadastrousuario/index.tsx
+++ src/components/cadastrousuario/index.tsx
@@ -11,42 +11,42 @@ function CadastroUsuario() {
   };
    
   
-    return (<div> 
-        <div className="CadastroUsuario-Container">
-        
-          <div className="botaologin-container">
-            
-              <div className="botaocadastrologin-form">
-                <h1 p style={{color:"white"}}>Onde vai Cadastro</h1>
-                <img className='fotologin' src="Icone Login.png" alt="login" />
-                <form>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Nome Completo"/>
-                  </div>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Email"/>
-                  </div>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="password" placeholder="Senha"/>
-                  </div>
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Telefone"/>
-                  </div>
-  
-                  <div className="botaocadastrologin-form-div1">
-                    <input className="botaocadastrologin-form-input" type="text" placeholder="Sexo"/>
-                  </div>
-  
-                  <button className="botaologin-submit" type="submit" >Cadastre-se</button>
-                </form>
-            
+  return (
+    <div>
+      <div className="CadastroUsuario-Container">
+        <div className="botaologin-container">
+          <div className="botaocadastrologin-form">
+            <h1 style={{ color: "white" }}>Onde vai Cadastro</h1>
+            <img className='fotologin' src="Icone Login.png" alt="login" />
+            <form>
+              <div className="botaocadastrologin-form-div1">
+                <input className="botaocadastrologin-form-input" type="text" placeholder="Nome Completo" />
+              </div>
+              <div className="botaocadastrologin-form-div1">
+                <input className="botaocadastrologin-form-input" type="text" placeholder="Email" />
+              </div>
+              <div className="botaocadastrologin-form-div1">
+                <input className="botaocadastrologin-form-input" type="password" placeholder="Senha" />
+              </div>
+              <div className="botaocadastrologin-form-div1">
+                <input className="botaocadastrologin-form-input" type="text" placeholder="Telefone" />
+              </div>
+
+              <div className="botaocadastrologin-form-div1">
+                <input className="botaocadastrologin-form-input" type="text" placeholder="Sexo" />
+              </div>
+
+              <button className="botaologin-submit" type="submit" >Cadastre-se</button>
+            </form>
+
             <button onClick={sairDaTela} className="fechar-btn"><img src="sair (3).png" alt="Sair" /></button>
-                </div>
-             
-            {/* )} */}
           </div>
-     </div></div>    )
-     }
+
+        </div>
+      </div>
+    </div>
+  )
+}
   
 
 
