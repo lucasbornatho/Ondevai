2bc3c8ea5a2dd0aa726f633b59df09ba94a7983e
Lucas Bornatho Pires <lucasbornatho@yahoo.com.br>
2025-04-22 21:19:14 -0300

    Modificacao infos home

--- src/components/infoshome/index.tsx
+++ src/components/infoshome/index.tsx
@@ -2,7 +2,7 @@ function InfosHome() {
     return (
         <div className="infos-menu">
             <div className="texto-menu">
-                <img src="./OndeVai Texto3.png" alt="sobre" />
+                <img src="./OndeVai Texto4.png" alt="sobre" />
             </div>
         </div>
     );
